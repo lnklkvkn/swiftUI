ff3795e6dbd0cfad90edb4a27bcd5c356754f5cd
lnklkvkn <lnklkvkn@mail.ru>
2023-07-10 20:42:24 +0300

    Initial Commit

---
+++ SwiftUIEducation/SwiftUIEducationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEducationApp.swift
+//  SwiftUIEducation
+//
+//  Created by ln on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEducationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
